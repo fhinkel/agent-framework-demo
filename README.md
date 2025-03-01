cd2e131de4d306817c82aeb15e5334dcfab0ab95
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-28 20:44:49 -0500

    With deploy script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 * Activate virtual env
 * Install go/agent-framework whel file. 
 * run with `fa run . ` 
+
+Currently deployed at projects/988266241228/locations/us-central1/reasoningEngines/4024793099799625728
--- agent.py
+++ agent.py
@@ -1,8 +1,85 @@
-from agents import Agent
-import random
-
+from typing import Any, Dict, List, Optional
+from google.genai import types
+from agents.sessions import Session
+from agents.sessions import InMemorySessionService
+from agents.events import Event
+from agents.artifacts import InMemoryArtifactService
+from agents import Runner
+import os
 import warnings
+import random
+from agents import Agent
+import vertexai
+from vertexai.preview import reasoning_engines
+
+PROJECT_ID = "af-deploy"
+LOCATION = "us-central1"
+RELEASE_VERSION = 'google_genai_agents-0.0.2.dev20250204+723246417'
+
+
 warnings.filterwarnings("ignore")
+
+os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "1"
+os.environ["GOOGLE_CLOUD_PROJECT"] = PROJECT_ID
+os.environ["GOOGLE_CLOUD_LOCATION"] = LOCATION
+
+
+session_service = InMemorySessionService()
+artifact_service = InMemoryArtifactService()
+
+
+def create_session(context):
+    session = session_service.create(
+        "Documentation Update Agent App", "user", context, session_id="demo_session_id ")
+    return session
+
+
+def run_prompt(session: Session,
+               runner: Runner,
+               message: str):
+    content = types.Content(
+        role='user', parts=[types.Part.from_text(text=message)])
+    for event in runner.run(
+        session=session,
+        new_message=content,
+    ):
+        if event.content:
+            print(event.content.model_dump(exclude_none=True))
+            pass
+
+
+agent_context = {
+    "company_name": 'Otis Worldwide Corporation',
+    # "company_name": 'Hinkelmann LLC'
+}
+
+
+class App:
+    def __init__(self, agent):
+        self.session_service = InMemorySessionService()
+        self.runner = Runner(
+            app_name="docs_update_agent",
+            agent=agent,
+            artifact_service=InMemoryArtifactService(),
+            session_service=self.session_service,
+        )
+
+    def agent_run(self, session_id: str, message: str):
+        os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "1"
+        os.environ["GOOGLE_CLOUD_PROJECT"] = "af-deploy"
+        os.environ["GOOGLE_CLOUD_LOCATION"] = "us-central1"
+        yield from self.runner.run(
+            session=self.session_service.create(
+                "docs_update_agent",
+                "user",
+                state=agent_context,
+                session_id=session_id,
+            ),
+            new_message=types.Content.model_validate_json(message),
+        )
+
+    def register_operations(self) -> Dict[str, List[str]]:
+        return {"stream": ["agent_run"]}
 
 
 def upload_to_youtube(filename: str, title: str, description: str) -> tuple[int, str]:
@@ -88,4 +165,30 @@ root_agent = Agent(
         update_code_snippet,
     ],
     flow='single',
+)
+
+
+vertexai.init(
+    project=PROJECT_ID,
+    location=LOCATION,
+    # api_endpoint="us-central1-autopush-aiplatform.sandbox.googleapis.com",
+    api_endpoint="us-central1-aiplatform.googleapis.com",
+    staging_bucket="gs://agent-engine-deploy-1",
+)
+
+remote_app = reasoning_engines.ReasoningEngine.create(
+    App(root_agent),
+    display_name="documentation_update_agent_app",
+    requirements=[
+        RELEASE_VERSION + "-py3-none-any.whl",
+        "google_cloud_aiplatform",
+        "google_genai",
+        "cloudpickle==3.1.1",
+        "pydantic==2.10.6",
+        "pytest",
+        "overrides",
+    ],
+    extra_packages=[
+        RELEASE_VERSION + "-py3-none-any.whl",
+    ],
 )
