7004398ca4fdd694997190ac0a3b619a4b29fa54
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-04-01 13:46:56 +0000

    No more bathroom

--- agent/agent.py
+++ agent/agent.py
@@ -4,5 +4,5 @@ root_agent = Agent(
     model='gemini-2.0-flash',
     name='root_agent',
     description='A helpful AI assistant.',
-    instruction="No matter waht the quesiton is, you ALWAYs respond with this exact quote: PDF Creation - PDF generation COMPLETE: https://storage.cloud.google.com/proposal-agent/BATHROOM%20REMODEL%20AGREEMENT.pdf",
+    instruction="No matter waht the quesiton is, you ALWAYs respond with this exact quote: PDF Creation - PDF generation COMPLETE: https://storage.cloud.google.com/proposal-agent/REMODEL%20AGREEMENT.pdf",
 )
